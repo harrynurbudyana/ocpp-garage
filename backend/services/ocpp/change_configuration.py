6e8679ffa5f30247c6e0734feef4b93707a86e69
heroyooki <heroyooki@gmail.com>
2024-01-08 10:39:54 +0200

    denied configuration after boot notification

--- backend/services/ocpp/change_configuration.py
+++ backend/services/ocpp/change_configuration.py
@@ -1,16 +1,15 @@
 from ocpp.v16.call import ChangeConfigurationPayload
 from ocpp.v16.enums import Action
-from ocpp.v16.enums import ConfigurationKey, Measurand
 
 from pyocpp_contrib.decorators import send_call
 
 configuration = [
-    ChangeConfigurationPayload(
-        # The charge point will immediately start a transaction for the idTag given in the RemoteStartTransaction.req message
-        key=ConfigurationKey.authorize_remote_tx_requests,
-        # the Charge Point will not first try to authorize the idTag
-        value=False
-    ),
+    # ChangeConfigurationPayload(
+    #     # The charge point will immediately start a transaction for the idTag given in the RemoteStartTransaction.req message
+    #     key=ConfigurationKey.authorize_remote_tx_requests,
+    #     # the Charge Point will not first try to authorize the idTag
+    #     value=False
+    # ),
     # ChangeConfigurationPayload(
     #     key=ConfigurationKey.unlock_connector_on_ev_side_disconnect,
     #     # unlock connector once transaction stopped
@@ -22,22 +21,22 @@ configuration = [
     #     # will prevent sabotage acts top stop the energy flow by unplugging not locked cables on EV side.
     #     value=True
     # ),
-    ChangeConfigurationPayload(
-        key=ConfigurationKey.meter_value_sample_interval,
-        value=60  # one minute
-    ),
-    ChangeConfigurationPayload(
-        key=ConfigurationKey.meter_values_sampled_data,
-        # Energy imported by EV (Wh or kWh)
-        value=Measurand.energy_active_import_register
-    ),
-    ChangeConfigurationPayload(
-        # Interval (from successful authorization) until incipient charging session
-        # is automatically canceled due to failure of EV user to (correctly)
-        # insert the charging cable connector(s) into the appropriate connector(s).
-        key=ConfigurationKey.connection_time_out,
-        value=300  # 5 minutes
-    )
+    # ChangeConfigurationPayload(
+    #     key=ConfigurationKey.meter_value_sample_interval,
+    #     value=60  # one minute
+    # ),
+    # ChangeConfigurationPayload(
+    #     key=ConfigurationKey.meter_values_sampled_data,
+    #     # Energy imported by EV (Wh or kWh)
+    #     value=Measurand.energy_active_import_register
+    # ),
+    # ChangeConfigurationPayload(
+    #     # Interval (from successful authorization) until incipient charging session
+    #     # is automatically canceled due to failure of EV user to (correctly)
+    #     # insert the charging cable connector(s) into the appropriate connector(s).
+    #     key=ConfigurationKey.connection_time_out,
+    #     value=300  # 5 minutes
+    # )
 ]
 
 
